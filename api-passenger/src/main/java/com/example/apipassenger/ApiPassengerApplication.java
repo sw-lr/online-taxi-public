68eaa20d49d1136a111e9c353f190b1419a85db0
yangsiwang <917672184@qq.com>
2025-04-24 16:41:34 +0800

    api-passenger项目骨架搭建

---
+++ api-passenger/src/main/java/com/example/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,20 @@
+package com.example.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: ApiPassengerApplication
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author 杨思旺
+ * @Create 2025/4/24 16:11
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ApiPassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
