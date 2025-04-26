bfa6673765243d8f67d9572ffdf144d6a424fc06
yangsiwang <917672184@qq.com>
2025-04-26 14:02:26 +0800

    nacos注册中心搭建，api-passenger测试

--- api-passenger/src/main/java/com/example/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/example/apipassenger/ApiPassengerApplication.java
@@ -2,6 +2,7 @@ package com.example.apipassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * ClassName: ApiPassengerApplication
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPassengerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiPassengerApplication.class);
